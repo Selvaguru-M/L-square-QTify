54b7dd1f33d0632a793a6046159763746ff550c4
chelindshree <devakanhashree@gmail.com>
2024-08-30 21:23:42 +0530

    navbar n logo

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import LogoImage from "../../assets/logo.png";
 import styles from "./Logo.module.css"
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} className={styles.logoheader}/>
+  return <img src={LogoImage} alt="logo" width={67} />
   // (<div className="Logo-header">
   //   <img  src={LogoImage} alt="logo" width={67} />
   //   </div>);
