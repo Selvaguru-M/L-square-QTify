c7de95e828ee8c8e032189a5f33bab55f1be579b
chelindshree <devakanhashree@gmail.com>
2024-08-30 21:00:07 +0530

    Logo

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import LogoImage from "../../assets/logo.png";
 // import "./Logo.module.css"
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} />
+  return <img src={LogoImage} alt="logo" width={67} className="Logo-header"/>
   // (<div className="Logo-header">
   //   <img  src={LogoImage} alt="logo" width={67} />
   //   </div>);
