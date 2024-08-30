21e96232bdbfe5dcb7def45fa7d933dac3ac7c79
chelindshree <devakanhashree@gmail.com>
2024-08-30 15:53:23 +0530

    Trail2

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
-// import "./Logo.module.css"
+import "./Logo.module.css"
 
 export default function Logo() {
   return <div className="Logo-header">
