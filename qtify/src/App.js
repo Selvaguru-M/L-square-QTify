02088ba02474ec573d673cb26f9ff387423ab413
chelindshree <devakanhashree@gmail.com>
2024-08-30 16:45:44 +0530

    Carousal and Filter tab

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from "./components/Hero/Hero";
+import Section  from "./components/Section/Section";
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         {/* Render Navbar at the top */}
         <Navbar />
         <Hero />
+        <Section />
       </div>
   );
 }
