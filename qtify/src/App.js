def595f2256beffea4de920bdf5fab4fe674dc2a
chelindshree <devakanhashree@gmail.com>
2024-08-30 17:00:39 +0530

    homepage and api

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,19 +1,69 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Hero from "./components/Hero/Hero";
-import Section  from "./components/Section/Section";
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
+import Logo from "./components/Logo/Logo";
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/Search/Search";
+import { Outlet } from "react-router-dom";
+import {
+  fetchNewAlbum,
+  fetchTopAlbum,
+  fetchFilters,
+  fetchSongs,
+} from "./components/api/api";
 
 function App() {
+  // const [searchData, SetSearchData] = useState([]);
+  const [data, setData] = useState({});
+  const generateData = (key, dataSource) => {
+    dataSource().then((data) => {
+      setData((prevData) => {
+        return { ...prevData, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbum", fetchTopAlbum);
+    generateData("newAlbum", fetchNewAlbum);
+    generateData("genres", fetchFilters);
+    generateData("songs", fetchSongs);
+  }, []);
+
+  const { topAlbum = [], newAlbum = [], genres = [], songs = [] } = data;
+
   return (
-   
-      <div className="App">
-        {/* Render Navbar at the top */}
-        <Navbar />
-        <Hero />
-        <Section />
-      </div>
+    <>
+      <Navbar searchData={[...topAlbum, newAlbum]}>
+        <Logo />
+        <Search placeholder={"Search a album of your choice"} />
+        <Button
+          text="Give Feedback"
+          onClick={() => alert("Feedback button clicked!")}
+        />
+      </Navbar>
+      <Outlet context={{ data: { topAlbum, newAlbum, genres, songs } }} />
+    </>
   );
 }
 
 export default App;
+// import './App.css';
+// import Navbar from './components/Navbar/Navbar';
+// import Hero from "./components/Hero/Hero";
+// // import Section  from "./components/Section/Section";
+
+
+// function App() {
+//   return (
+   
+//       <div className="App">
+//         {/* Render Navbar at the top */}
+//         <Navbar />
+//         <Hero />
+//         {/* <Section /> */}
+//       </div>
+//   );
+// }
+
+// export default App;
--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
-import "./Logo.module.css"
+// import "./Logo.module.css"
 
 export default function Logo() {
-  return <div className="Logo-header">
-    <img  src={LogoImage} alt="logo" width={67} />
-    </div>;
+  return <img src={LogoImage} alt="logo" width={67} />
+  // (<div className="Logo-header">
+  //   <img  src={LogoImage} alt="logo" width={67} />
+  //   </div>);
 }
