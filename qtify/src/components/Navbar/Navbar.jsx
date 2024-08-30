c347e3a1b73f081fcd853d57252fdaad318772e7
chelindshree <devakanhashree@gmail.com>
2024-08-30 21:30:40 +0530

    navbar

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
@@ -8,9 +8,9 @@ import styles from "./Navbar.module.css";
 function Navbar({ searchData }) {
   return (
     <nav className={styles.navbar}>
-      {/* <Link to="/"> */}
+      <Link to="/">
         <Logo />
-      {/* </Link> */}
+      </Link>
       <Search 
         placeholder="Search a song of your choice"
         searchData={searchData}
