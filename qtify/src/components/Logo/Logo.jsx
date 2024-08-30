d3c09668259f16709bf4452b47b0d8171179fcd2
chelindshree <devakanhashree@gmail.com>
2024-08-30 21:14:54 +0530

    Logo

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
-// import "./Logo.module.css"
+import styles from "./Logo.module.css"
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} style={{ paddingLeft: '1px' }}/>
+  return <img src={LogoImage} alt="logo" width={67} className={styles.logoheader}/>
   // (<div className="Logo-header">
   //   <img  src={LogoImage} alt="logo" width={67} />
   //   </div>);
